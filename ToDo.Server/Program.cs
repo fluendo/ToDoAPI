5d9db7c34c06c65c093294f3f7b18160ec75c41c
Andoni Morales Alastruey <ylatuya@gmail.com>
2017-01-25 15:39:17 +0100

    Initial commit

---
+++ ToDo.Server/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.Owin.Hosting;
+
+namespace TodoApi
+{
+	public class Program
+	{
+		protected static void Main(string[] args)
+		{
+			var port = 8080;
+			var url = $"http://localhost:{port}";
+			using (WebApp.Start<Startup>(url))
+			{
+				Console.WriteLine($"Web Server is running at {url}.");
+				Console.WriteLine("Press any key to quit.");
+				Console.ReadLine();
+			}
+		}
+	}
+}
