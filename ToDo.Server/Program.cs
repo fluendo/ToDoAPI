2e9cd6b1d16e173e1da8cd698f6e46bb990717c4
Gustavo Jimenez <gustavoja@gmail.com>
2017-08-19 01:45:36 +0200

    Check if port is being used by the system before start  to avoid exceptions

--- ToDo.Server/Program.cs
+++ ToDo.Server/Program.cs
@@ -1,20 +1,54 @@
 ﻿using System;
 using Microsoft.Owin.Hosting;
+using System.Net.NetworkInformation;
+using System.Net;
 
 namespace TodoApi
 {
-	public class Program
-	{
-		protected static void Main(string[] args)
-		{
-			var port = 8080;
-			var url = $"http://localhost:{port}";
-			using (WebApp.Start<Startup>(url))
-			{
-				Console.WriteLine($"Web Server is running at {url}.");
-				Console.WriteLine("Press any key to quit.");
-				Console.ReadLine();
-			}
-		}
-	}
+    public class Program
+    {
+        protected static void Main(string[] args)
+        {
+            var port = 8080;
+            var url = $"http://localhost:{port}";
+
+            if (IsPortAvailable(port))
+            {
+                using (WebApp.Start<Startup>(url))
+                {
+                    Console.WriteLine($"Web Server is running at {url}.");
+                    Console.WriteLine("Press any key to quit.");
+                    Console.ReadLine();
+                }
+            }
+            else
+            {
+                Console.WriteLine($"The port {port} is being used by your system. Try with another port");
+                Console.WriteLine("Press any key to quit.");
+                Console.ReadLine();
+            }
+
+
+        }
+
+        /// <summary>
+        /// Check if the port is being used by the system
+        /// </summary>
+        /// <param name="port">TCP port</param>
+        /// <returns></returns>
+        private static bool IsPortAvailable(int port)
+        {
+            IPGlobalProperties ipGlobalProperties = IPGlobalProperties.GetIPGlobalProperties();
+            IPEndPoint[] tcpListenersEndPoints = ipGlobalProperties.GetActiveTcpListeners();
+
+            foreach (var tcpEp in tcpListenersEndPoints)
+            {
+                if (tcpEp.Port == port)
+                    return false;
+            }
+
+            return true;
+        }
+    }
 }
+
